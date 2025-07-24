8a489d3c6968565da633ca0aced19bd2cc2ca7f0
Vikas-Gn <vikasgn2000@gmail.com>
2025-07-24 15:56:18 +0530

    updated-code

--- Backend/server.js
+++ Backend/server.js
@@ -813,7 +813,6 @@ app.use((err, req, res, next) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+app.listen(3001, '0.0.0.0', () => {
+    console.log(`Server running on port 3001`);
 });
